5b3b90866d56037fc1275427b31b83ea8a20ef92
jihoLee0818 <leejiho0114@gmail.com>
2025-08-28 09:03:44 +0900

    [level 1] Title: 명예의 전당 (1), Time: 12.01 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer =new int[score.length];
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<score.length; i++){
+            list.add(score[i]);
+
+            Collections.sort(list);
+
+            answer[i]=(i>=k? list.get(i-k+1):list.get(0));
+        }
+        return answer;
+    }
+}
