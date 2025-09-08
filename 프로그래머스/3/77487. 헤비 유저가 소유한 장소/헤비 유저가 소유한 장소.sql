a50bc310f609f27106e8ab54a17b281b770b1d87
jihoLee0818 <leejiho0114@gmail.com>
2025-09-08 09:03:46 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,11 @@
+SELECT p.ID,
+       p.NAME,
+       p.HOST_ID
+FROM PLACES p
+WHERE p.HOST_ID IN (
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(*) >= 2
+)
+ORDER BY p.ID
