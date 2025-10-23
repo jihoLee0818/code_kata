e08c828dd22be4b5c4293981088b319d20c3762b
jihoLee0818 <leejiho0114@gmail.com>
2025-10-23 09:01:29 +0900

    Time: 515 ms (67.57%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ LeetCode/Medium/0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,12 @@
 SELECT DISTINCT
-    l1.num AS ConsecutiveNums
-FROM
-    Logs l1
-JOIN
-    Logs l2 ON l1.id = l2.id - 1
-JOIN
-    Logs l3 ON l2.id = l3.id - 1
+    num AS ConsecutiveNums
+FROM (
+    SELECT
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num
+    FROM
+        Logs
+) AS SubQuery
 WHERE
-    l1.num = l2.num AND l2.num = l3.num;
+    num = prev_num AND num = next_num;
