90f69d1a020107746182c3641e0f1ac3dfd8a207
jihoLee0818 <leejiho0114@gmail.com>
2026-02-01 22:40:25 +0900

    [level 0] Title: 특수문자 출력하기, Time: 122.97 ms, Memory: 64.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // 출력해야 할 내용: !@#$%^&*(\'"<>?:;
+        
+        // 1. \ (백슬래시)는 \\로 적어야 합니다.
+        // 2. " (큰따옴표)는 \"로 적어야 합니다.
+        // 3. ' (작은따옴표)는 문자열 안에서 그냥 적어도 됩니다.
+        
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
