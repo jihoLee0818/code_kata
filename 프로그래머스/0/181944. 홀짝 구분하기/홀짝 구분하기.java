554c2992977cac1bd5adb83a18529fef7a0e3baa
jihoLee0818 <leejiho0114@gmail.com>
2026-02-03 01:15:12 +0900

    [level 0] Title: 홀짝 구분하기, Time: 169.42 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        // n을 2로 나눈 나머지가 0이면 짝수, 아니면 홀수
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
+}
